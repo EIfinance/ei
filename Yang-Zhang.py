d75d126774084b21efd8cca2e08bab5a035f72ef
hugovigna <hugo.vigna@student-cs.fr>
2024-06-03 14:49:51 +0200

    Update Yang-Zhang.py

--- Yang-Zhang.py
+++ Yang-Zhang.py
@@ -15,23 +15,3 @@ def calculate_yang_zhang(data, k):
                   + np.log(data['Low'] / data['Close']) * np.log(data['Low'] / data['Open'])))
     
     return yang_zhang
-
-def plot_yang_zhang_variance(data):
-    """
-    Trace la variance de l'indicateur Yang-Zhang en fonction de k.
-    """
-    k_values = np.linspace(0, 1, 100)
-    variances = []
-
-    for k in k_values:
-        yang_zhang = calculate_yang_zhang(data, k)
-        variances.append(np.var(yang_zhang))
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(k_values, variances, label='Variance de Yang-Zhang')
-    plt.xlabel('k')
-    plt.ylabel('Variance')
-    plt.title('Variance de l\'indicateur Yang-Zhang en fonction de k')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
